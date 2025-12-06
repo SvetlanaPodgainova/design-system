8419ad4161706244c3ab9a02140375d22e1f87b2
SPodgainova <spodgainova@gmail.com>
2025-12-06 17:53:43 +0300

    import fix

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,5 +1,4 @@
-// import { UserMenu } from "../../entities/user/ui/UserMenu/UserMenu";
-import { CurrentUser } from "../../features/CurrentUser/CurrentUser";
+import { CurrentUser } from "../../features/current-user/ui/CurrentUser";
 import { ThemeToggle } from "../../features/ThemeToggle/ThemeToggle";
 import { Logo } from "../../shared/ui/Logo/Logo";
 import styles from "./Header.module.css";
@@ -10,8 +9,7 @@ export const Header = () => {
       <div className={styles.inner}>
         <Logo />
         <ThemeToggle />
-        <CurrentUser />
-        {/* <UserMenu displayName="Svetlana" /> */}
+        <CurrentUser />      
       </div>
     </header>
   );
